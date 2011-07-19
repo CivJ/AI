5307c031ce10edea356a657d2b97d380213a8b57
Ballfry <john.crimmins@gmail.com>
2011-07-19 18:53:45 -0400

    Fixed bug in environment causing agent to not move properly.

--- ai/simpleVacuumAgent/src/main/clojure/Environment.clj
+++ ai/simpleVacuumAgent/src/main/clojure/Environment.clj
@@ -39,7 +39,8 @@ All possible environment states are in the sequence.We are Using vectors so we c
         max (- (count (get-board)) 1)]
   (cond
    (< location min) (update-state (get-board) min)
-   (> location max) (update-state (get-board) max ))))
+   (> location max) (update-state (get-board) max )
+   true (update-state (get-board) location))))
     
 ;assoc only works on maps and vectors.
 (defn get-cleaned-environment [location]
